903d4d525f2a7840c66c1c060c054c4f86237039
Thomas Harrison <thomas.harrison666@gmail.com>
2020-02-01 19:33:26 +0000

    part 15

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
-import logo from './logo.svg';
-import PageWrapper from './components/PageWrapper';
-import Home from './components/Pages/Home';
 import './App.css';
 
-function App() {
-  return (
-    <Router>
-      <PageWrapper>
+// Pages
+import PageWrapper from './components/PageWrapper';
+import Home from './components/Pages/Home';
+import About from './components/Pages/About';
 
-        <Route 
-          path="/"
-          component={Home}
-        />
-        
-      </PageWrapper>
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <PageWrapper>
+  
+          <Route 
+            exact = {true}
+            path="/"
+            component={Home}
+          />
+
+          <Route
+            path="/about"
+            component={About}
+          />
+        </PageWrapper>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,19 +1,20 @@
 import React, {Component} from 'react';
+import Header from '../Common/Header';
+import image from '../assets/images/home.jpg';
 
 class Home extends Component {
 
     render(){
         return(
             <div>
-                  <header className="masthead">
-                    <div className="container">
-                    <div className="intro-text">
-                        <div className="intro-lead-in">Welcome To Our Studio!</div>
-                        <div className="intro-heading text-uppercase">It's Nice To Meet You</div>
-                        <a className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">Tell Me More</a>
-                    </div>
-                    </div>
-                </header>
+                <Header
+                    title="Welcome to our website!"
+                    subtitle="website subtitle"
+                    buttonText="More Info"
+                    link="/services"
+                    showButton={true}
+                    image={image}
+                />
             </div>
         );
     }
